0cc6ad7389cfe907244beecb761b9fc45d6c5949
MiRene15 <ladymirene@proton.me>
2025-10-04 11:23:25 +0800

    Created PurchaseOrderController

---
+++ Controller/PurchaseOrderController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoeShop.Services.Interfaces;
+using ShoeShop.Services.DTOs;
+using System;
+
+namespace ShoeShop.Controllers
+{
+    [Authorize(Roles = "Admin,Manager")]
+    public class PurchaseOrderController : Controller
+    {
+        private readonly IPurchaseOrderService _orderService;
+
+        public PurchaseOrderController(IPurchaseOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _orderService.GetAllOrders();
+            return View(orders);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreatePurchaseOrderDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            _orderService.CreatePurchaseOrder(dto);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Confirm(int id)
+        {
+            _orderService.ConfirmOrder(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Receive(int id)
+        {
+            _orderService.ReceiveOrder(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var order = _orderService.GetOrderById(id);
+            return View(order);
+        }
+    }
+}
