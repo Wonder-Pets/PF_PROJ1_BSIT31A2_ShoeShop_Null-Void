c531c41999c85a9e8ea992a8a3108c139948e8c2
MiRene15 <ladymirene@proton.me>
2025-10-04 11:29:38 +0800

    MVC Project

---
+++ StudentD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
