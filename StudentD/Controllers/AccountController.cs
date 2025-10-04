0d22fc0122cfc4a0263f6436f470f310a41f044e
MiRene15 <ladymirene@proton.me>
2025-10-04 11:35:27 +0800

    Created AccountController

---
+++ StudentD/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoeShop.Models;
+using ShoeShop.Services.DTOs;
+using System.Threading.Tasks;
+
+namespace ShoeShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            if (!ModelState.IsValid) return View(dto);
+
+            var result = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, dto.RememberMe, false);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Dashboard");
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(dto);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (!ModelState.IsValid) return View(dto);
+
+            var user = new ApplicationUser
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName
+            };
+
+
+
